c9025aad2912f255bdfd3486e78bd9f96151af93
DennySim <38099544+DennySim@users.noreply.github.com>
2018-05-22 18:00:13 +0300

    Add files via upload

--- metrikaclient.py
+++ metrikaclient.py
@@ -13,6 +13,7 @@ auth_data = dict(
 print('Enter your token')
 TOKEN = input()
 
+
 class YaMetrikaManagement:
 
     def __init__(self, token):
@@ -23,15 +24,18 @@ class YaMetrikaManagement:
 
     @property
     def counters(self):
-        response = requests.get('https://api-metrika.yandex.ru/management/v1/counters',
-                                headers=self.get_headers()
-                                )
+        response = requests.get(
+            'https://api-metrika.yandex.ru/management/v1/counters',
+            headers=self.get_headers()
+        )
         return [c['id'] for c in response.json()['counters']]
 
     def get_counter_info(self, counter_id):
-        response = requests.get('https://api-metrika.yandex.ru/management/v1/counter/{}'.format(counter_id),
-                                headers=self.get_headers()
-                                )
+        response = requests.get(
+            ('https://api-metrika.yandex.ru/management/v1/counter/{}'
+                .format(counter_id)),
+            headers=self.get_headers()
+        )
         return response.json()
 
 
@@ -48,14 +52,16 @@ class Reports:
     def report(self):
 
             response = requests.get(
-                'https://api-metrika.yandex.ru/stat/v1/data?id={}&metrics=ym:s:visits,ym:s:pageviews,ym:s:users'
-                .format(self.counter_id),
+                ('https://api-metrika.yandex.ru/stat/v1/data?id={}&'
+                    'metrics=ym:s:visits,ym:s:pageviews,ym:s:users'
+                    .format(self.counter_id)),
                 headers=self.get_headers()
             )
             return response.json()['data']
 
 
 ya_user1 =YaMetrikaManagement(TOKEN)
+
 
 def print_output():
     for counter_id in ya_user1.counters:
@@ -65,5 +71,6 @@ def print_output():
         print('Визиты', metriki[0])
         print('Просмотры', metriki[1])
         print('Посетители', metriki[2])
+
 
 print_output()
